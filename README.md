85ca5c8dd9f41ac05c76282f2074812b9705b23f
QichenJ <QichenJ@JingQichens-MBP.hsd1.ca.comcast.net>
2015-09-03 18:32:09 -0700

    refine code, add readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
-
-for self-checking their submission.
+This is arcade game.
+1. Wen the game starts, the player will stand at the middle of the grass.
+2. Enemies will appear on the road, there are three enemies in total, their speed is randomly but will not be too fast.
+3.You can use 'up', 'down', 'left' and 'right' on keyboards to control your character.
+4.You can not go outside of the board.
+5.You need to avoid enemies, if you collide a enemy, the player will die and reset to starting poitn.
+6.When player reach the river, you win.
--- js/app.js
+++ js/app.js
@@ -7,16 +7,17 @@ var Enemy = function() {
     // a helper we've provided to easily load images
 
     //x should be from 0 to 4, y should be 2 to 4
-    this.x = randomLen(5, 0, 101);
-    this.y = randomLen(3, 1, 83) - 35;
+    this.x = randomLen(5, 0, XSTEP);
+    this.y = randomLen(3, 1, YSTEP) - 35;
     this.speed = Math.floor((Math.random() * 3 + 2) * 110);
     this.sprite = 'images/enemy-bug.png';
 };
-var height = 606;
-var width = 505;
+var WIDTH = 505;
+var XSTEP = 101;
+var YSTEP = 83;
 var randomLen = function(num, bias, length) {
     return (Math.floor(Math.random() * num) + bias)* length;
-}
+};
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -24,9 +25,9 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
-    if(this.x > width) {
-        this.x = -randomLen(3, 1, 101);
-        this.y = randomLen(3, 1, 83) - 35;
+    if(this.x > WIDTH) {
+        this.x = -randomLen(3, 1, XSTEP);
+        this.y = randomLen(3, 1, YSTEP) - 35;
         this.speed = Math.floor((Math.random() * 3 + 2) * 110);
     }
     this.check();
@@ -41,16 +42,16 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-    this.x = 101 * 2;
-    this.y = 83 * 4 + 40;
-    this.speedx = 101;
-    this.speedy = 83;
+    this.x = XSTEP * 2;
+    this.y = YSTEP * 4 + 40;
+    this.speedx = XSTEP;
+    this.speedy = YSTEP;
     this.sprite = 'images/char-boy.png';
-}
+};
 
 Enemy.prototype.check = function() {
     checkCollision(this, player, 20, 20);
-}
+};
 
 Player.prototype.update = function(code) {
     switch(code) {
@@ -74,34 +75,34 @@ Player.prototype.update = function(code) {
         }
         default: break;
     }
-}
+};
 
 var reset = function() {
-    player.x = 202;
-    player.y = 83 * 4 + 40;
-}
+    player.x = XSTEP * 2;
+    player.y = YSTEP * 4 + 40;
+};
 
 var checkCollision = function(e, p, xlen, ylen) {
     if(Math.abs(p.x - e.x) < xlen && Math.abs(p.y - e.y) < ylen) {
         reset();
     }
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 Player.prototype.handleInput = function(key) {
     this.update(key);
-}
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 var allEnemies = [];
-allEnemies.push(new Enemy());
-allEnemies.push(new Enemy());
-allEnemies.push(new Enemy());
+for(var i = 0; i < 3; i++) {
+    allEnemies.push(new Enemy());
+}
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
